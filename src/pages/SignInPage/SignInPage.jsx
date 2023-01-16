a20d5ef2a61277ac407fac9625a228226dc9dee7
DimaDzyuba <dima.dzyuba.96@gmail.com>
2023-01-16 20:52:45 +0200

    init

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,31 +1,12 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
 import { Header } from '../../components/Header/Header'
-import styles from './styles.module.scss'
+import { SignInForm } from '../../components/Forms/SignInForm'
 
 const SignInPage = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm({
-    mode: 'onBlur'
-  })
-  const onSubmit = data => console.log(data)
-  console.log(watch('name'))
   return (
       <>
           <Header />
-          <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-              <input type='text' placeholder="Фамилия и имя"
-                     {...register('userName')}
-              />
-              <input placeholder="Почта" {...register('email', { required: 'Неверный email' })} />
-              <input placeholder="Пароль" {...register('pass')} />
-
-              {/* include validation with required or other standard HTML validation rules */}
-              <input placeholder="Еще раз пароль"{...register('againPass', { required: true })} />
-              {/* errors will return when field validation fails  */}
-              {errors.againPass && <span>Неверный пароль</span>}
-
-              <input type="submit" />
-          </form>
+          <SignInForm />
       </>
   )
 }
