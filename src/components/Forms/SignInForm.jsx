82c3e0b79fc57580d7aed228a6158aa37439cfd2
DimaDzyuba <dima.dzyuba.96@gmail.com>
2023-01-17 05:36:36 +0200

    update PC

--- src/components/Forms/SignInForm.jsx
+++ src/components/Forms/SignInForm.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import styles from '../../pages/SignInPage/styles.module.scss'
 import { useForm } from 'react-hook-form'
+import classNames from 'classnames'
 
 export const SignInForm = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm({
-    mode: 'onBlur'
+  const { register, handleSubmit, formState: { errors }, formState } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      userName: '',
+      email: '',
+      pass: ''
+    }
   })
   const onSubmit = data => console.log(data)
-  console.log(watch('userName'))
+  console.log(errors, formState)
   return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
             <div className={styles.form__wrapper}>
                 <h1 className={styles.form__title}>регистрация
                     <br/>В Отмечалочке<span>.</span>
                 </h1>
-                <input className={styles.form__input} type='text' placeholder="Фамилия и имя" {...register('userName')} />
-                <input className={styles.form__input} type='email' placeholder="Почта" {...register('email', { required: 'Неверный email' })} />
+                <input className={classNames(styles.form__input, styles.form__inputName, { [styles.form__errorInput]: errors.userName })}
+                       type='text'
+                       placeholder="Имя и Фамилия"
+                       {...register('userName', { required: 'Введите Имя и Фамилию' })} />
+                {errors.userName && <div className={styles.form__errorInputText}>{errors.userName.message}</div>}
+
+                <input className={classNames(styles.form__input, { [styles.errorInput]: errors.email })} type='email' placeholder="Почта" {...register('email', { required: 'Неверный email' })} />
+                {errors.email && <div className={styles.form__errorInputText}>{errors.email.message}</div>}
+
                 <input className={styles.form__input} type='password' placeholder="Пароль" {...register('pass')} />
 
-                {/* include validation with required or other standard HTML validation rules */}
                 <input className={styles.form__input} type='password' placeholder="Еще раз пароль"{...register('againPass', { required: true })} />
-                {/* errors will return when field validation fails  */}
-                {errors.againPass && <span className={styles.form__errors_againPass}>Неверный пароль</span>}
 
-                <button className={styles.form__button} placeholder='регистрация'>
+                {errors.againPass && <span className={styles.form__errors_againPass}>Неверный пароль</span>}
+                <button className={styles.form__button} placeholder='регистрация' onSubmit={handleSubmit(onSubmit)}>
                     Регистрация
                 </button>
             </div>
