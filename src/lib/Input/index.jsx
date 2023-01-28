ff7d78cad8967735351d89fe3c0d44b04c634466
DimaDzyuba <dima.dzyuba.96@gmail.com>
2023-01-28 16:13:48 +0200

    laptop

--- src/lib/Input/index.jsx
+++ src/lib/Input/index.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './style.module.scss'
 import { classNames } from '../../utils/classNames'
 
-export const Input = ({ type, placeholder, value, onChange }) => {
+export const Input = ({ type, placeholder, value, onChange, error }) => {
   const className = classNames(
-    styles.input
-
+    styles.input,
+    {
+      inputPass: type === 'password',
+      [styles.email]: type === 'email',
+      [styles.errors]: error
+    }
   )
+
   return (
-      <div>
+      <div className={className}>
+          {error || null}
           <input
               type={type}
               placeholder={placeholder}
               value={value}
               onChange={onChange}
-              className={className}
           />
       </div>
   )
